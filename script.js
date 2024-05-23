ad8e4f2a44e2b5da88869d380db319591e1f0638
Anthony <nitinbhagatxyz2@gmail.com>
2024-05-23 23:23:32 +0530

    On hover cursor code completed

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let cursor = document.getElementById("#cursor");
-let main = document.getElementById("#main");
-let center = document.getElementById("#center");
+let cursor = document.getElementById("cursor");
+let main = document.getElementById("main");
+let center = document.getElementById("center");
 
-document.addEventListener("mousemove", (e)=>{
+main.addEventListener("mousemove", (e)=>{
     gsap.to("#cursor", {
         x: e.clientX,
         y: e.clientY,
+    })
+})
+
+center.addEventListener("mouseenter", ()=>{
+    cursor.innerText = "Click";
+    gsap.to("#cursor", {
+        duration: 1,
+        scale: 5,
+        opacity: .5,
+        ease: "power2.out",
+    })
+})
+
+center.addEventListener("mouseleave", ()=>{
+    cursor.innerText = "";
+    gsap.to("#cursor", {
+        duration: 1,
+        scale: 1,
+        opacity: 1,
+        ease: "power2.out",
     })
 })
